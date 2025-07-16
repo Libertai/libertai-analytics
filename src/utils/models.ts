b34fba00a739b8a1edbd13e7709441ec944d73cd
Molaryy <molaryyios@gmail.com>
2025-07-16 10:53:19 +0200

    fix: fixed key into url to get new models

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import { Model } from "@/types/models";
 
 export const getModelsNames = async (): Promise<string[]> => {
-	const url = `https://api2.aleph.im/api/v0/aggregates/0xe1F7220D201C64871Cefb25320a8a588393eE508.json?keys=TEST_LTAI_PRICING`
+	const url = `https://api2.aleph.im/api/v0/aggregates/0xe1F7220D201C64871Cefb25320a8a588393eE508.json?keys=LTAI_PRICING`
 	const response = await axios.get(url).then((res) => res.data);
 	const models: string[] = [];
 
-	if (response.data && response.data.TEST_LTAI_PRICING && response.data.TEST_LTAI_PRICING.models) {
-		const gotModels =  response.data.TEST_LTAI_PRICING.models;
+	if (response.data && response.data.LTAI_PRICING && response.data.LTAI_PRICING.models) {
+		const gotModels =  response.data.LTAI_PRICING.models;
 
 		gotModels.forEach((model: Model) => {
 			models.push(model.id);
